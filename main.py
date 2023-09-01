9069160352adcb8801d386006b081ccb8cca64e8
Berkay Aksu <bekoberk5361@gmail.com>
2023-09-01 16:34:17 +0300

    interactiveDrawingTurtle

--- main.py
+++ main.py
@@ -7,22 +7,29 @@ drawing_board.title("interactive drawing board")
 turtle_instance = turtle.Turtle()
 
 def turtle_forward():
-    turtle_instance.right(90)
-    turtle_instance.forward(100)
+    turtle_instance.right(45)
 def sola():
-    turtle_instance.left(90)
-    turtle_instance.forward(100)
+    turtle_instance.left(45)
 def düz():
     turtle_instance.forward(100)
-def ters():
-    turtle_instance.left()
-    turtle_instance.forward(100)
+
+def clear_screen():
+    turtle_instance.clear()
+
+def return_home():
+    turtle_instance.home()
+def pen_up():
+    turtle_instance.penup()
+def pen_down():
+    turtle_instance.pendown()
 
 
 drawing_board.listen()
 drawing_board.onkey(fun=turtle_forward,key="d")
 drawing_board.onkey(fun=sola,key="a")
 drawing_board.onkey(fun=düz,key="w")
-drawing_board.onkey(fun=ters,key="s")
-
+drawing_board.onkey(fun=clear_screen,key="c")
+drawing_board.onkey(fun=return_home,key="h")
+drawing_board.onkey(fun=pen_up,key="e")
+drawing_board.onkey(fun=pen_down,key="q")
 turtle.mainloop()
